b9b64c1fba38629274730e8989bca3313e52f42b
david30-maker <davidokpah396@gmail.com>
2024-01-05 20:36:15 +0100

    Save searches along with user ip address

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,11 +6,12 @@ class SearchesController < ApplicationController
 
   def create
     search_term = params[:search_term]
-    last_search = Search.where(user_ip: params[:user_ip]).last
+    last_search = Search.where(user_ip: request.remote_ip).last
+    debugger
     if last_search && search_term.present? && search_term.start_with?(last_search&.search_term)
       last_search.update(search_term: search_term)
     elsif search_term.present?
-      Search.create(search_term: search_term, user_ip: params[:user_ip])
+      Search.create(search_term: search_term, user_ip: request.remote_ip)
     end
     head :ok
   end
