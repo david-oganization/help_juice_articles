9b09fcfe1ea0a2975ebd276096c69fae5ba7ba65
david30-maker <davidokpah396@gmail.com>
2024-01-05 22:32:56 +0100

    Handle logging every search but showing only complete search term in analytics

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,10 +1,18 @@
 class AnalyticsController < ApplicationController
   def show
-    @top_searches = Search.group(:search_term).order('count_id DESC').limit(10).count(:id)
-
+    query = "SELECT search_term, COUNT(search_term)
+            FROM searches s1
+            WHERE NOT EXISTS (
+              SELECT 1
+              FROM searches s2
+              WHERE s2.search_term > s1.search_term
+                AND position(s1.search_term in s2.search_term) = 1)
+            GROUP BY search_term
+            ORDER BY COUNT(search_term) DESC"
+    @top_searches = ActiveRecord::Base.connection.execute(query).to_a
+    
     respond_to do |format|
       format.html { render partial: 'show' }
-      # format.js
     end
   end
 end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,12 +6,11 @@ class SearchesController < ApplicationController
 
   def create
     search_term = params[:search_term]
-    last_search = Search.where(user_ip: request.remote_ip).last
-    if last_search && search_term.present? && search_term.start_with?(last_search&.search_term)
-      last_search.update(search_term: search_term)
-    elsif search_term.present?
+    if search_term.present?
       Search.create(search_term: search_term, user_ip: request.remote_ip)
     end
     head :ok
   end
+
+  # whitelist params in all controllers
 end
