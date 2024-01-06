2d8892a6f30927ac644ac581f3db46aca37f256c
david30-maker <davidokpah396@gmail.com>
2024-01-06 08:00:58 +0100

    Modify searches controller spec

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -2,18 +2,22 @@ require 'rails_helper'
 
 RSpec.describe SearchesController, type: :controller do
   describe 'POST #create' do
-    it 'creates a new search with valid search term' do
-      post :create, params: { search_term: 'example' }
-      expect(Search.count).to eq(1)
-      expect(Search.last.search_term).to eq('example')
-      expect(Search.last.user_ip).to eq(request.remote_ip)
-      expect(response).to have_http_status(:ok)
+    context 'with valid search term' do
+      it 'creates a new search' do
+        post :create, params: { search_term: 'example' }
+        expect(Search.count).to eq(1)
+        expect(Search.last.search_term).to eq('example')
+        expect(Search.last.user_ip).to eq(request.remote_ip)
+        expect(response).to have_http_status(:ok)
+      end
     end
 
-    it 'does not create a new search without search term' do
-      post :create, params: { search_term: '' }
-      expect(Search.count).to eq(0)
-      expect(response).to have_http_status(:ok)
+    context 'with an invalid search term' do
+      it 'does not create a new search' do
+        post :create, params: { search_term: '' }
+        expect(Search.count).to eq(0)
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 end
